a32c62347a69b7abd8b4d6dea1a877d6b202a2d9
Abhi <challaabhinayreddy@gmail.com>
2024-07-30 20:00:12 -0400

    my 2nd code

---
+++ code2.py
@@ -0,0 +1,16 @@
+A=6
+B=8
+Ans1=A+B
+print("Sum of two numbers",Ans1)
+Ans2=A-B
+print("Subtraction of two numbers",Ans2)
+Ans3=A*B
+print("multiplication of two numbers",Ans3)
+Ans4=A/B
+print("Division of two numbers",Ans4)
+Ans5=A%B
+print("modulus of two numbers",Ans5)
+Ans6=A**B
+print("Exponentiation of two numbers",Ans6)
+Ans7=A//B
+print("Floor division of two numbers",Ans7)
