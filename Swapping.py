608990d0a50e871f0bd407c1c220769a3d6cbbb8
Abhi <challaabhinayreddy@gmail.com>
2024-08-01 23:28:54 -0400

    Swapping

---
+++ Swapping.py
@@ -0,0 +1,10 @@
+A= int(input("Enter any First number = "))
+B=int(input("Enter any Second number =  "))
+print("First number",A)
+print("Second number",B)
+C=A
+A=B
+B=C
+print("After Swapping of two numbers")
+print("First number",A)
+print("Second number",B)
