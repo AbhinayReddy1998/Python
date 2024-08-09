b617c8e59a0817ce88102c4ff7c26413deb1691d
Abhi <challaabhinayreddy@gmail.com>
2024-08-09 15:14:03 -0400

    learning how to use function

---
+++ function.py
@@ -0,0 +1,23 @@
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def greaternumber(a,b):
+    if(a>b):
+        print("First number is greater = ",a)
+    else:
+        print("Secound number is greater = ",b)
+def lesser(a,b):
+    pass # it is used pass the function so that it wont show any error. if u dont use pass it throws an to insert something in function.
+    
+a =8
+b=9
+calculateGmean(a,b)
+greaternumber(a,b)
+#gmean1=(a*b)/(a+b)
+#print(gmean1)
+c=19
+d=8
+calculateGmean(c,d)
+greaternumber(c,d)
+#gmean2=(c*d)/(c+d)
+#print(gmean2)
