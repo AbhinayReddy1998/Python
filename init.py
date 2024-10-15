1c2be5a3a96548a6c9b44b7faa450ae6293f5f4e
Abhi <124208200+AbhinayReddy1998@users.noreply.github.com>
2024-10-15 13:56:44 -0400

    Add files via upload
    what is mean by init

---
+++ init.py
@@ -0,0 +1,8 @@
+class computer:
+    def __init__(self):
+        print("Abhinay")#here init is used to call automatically.When passing parameters to the __init__ method, the object itself is automatically passed as the first parameter
+    def config(self):
+        print("reddy")#Assigning values to object attributes using the passed parameters by using the 'self' keyword
+
+com1=computer()#object creation
+com1.config()
